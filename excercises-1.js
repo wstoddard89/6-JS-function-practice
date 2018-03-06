5a5c03595a6c8b287becf4893861f19c3c745946
Michael Sweeney <jfmike@gmail.com>
2018-03-05 16:25:46 -0800

    added assertions

--- excercises-1.js
+++ excercises-1.js
@@ -19,27 +19,38 @@
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 
+// READ FIRST
+// The assertions are there only to help you make sure 
+// your functions are written correctly. There is no need
+// to change them. If you write your function correctly,
+// the assertions will do nothing. If you write your function
+// incorrectly or not at all, your assertions will fail
+// in your console.
+
 
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
-
-
+console.assert(sum(2,4) === 6)
+console.assert(sum(45, 23) === 68)
+console.assert(sum(-2, -57) === -59)
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-
-
+console.assert(avg(2,3,4) === 3)
+console.assert(avg(234, 63, 234) === 177)
+console.assert(avg(123,234,345) === 234)
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
-
-
+console.assert(getLength('Michael') === 7)
+console.assert(getLength('Javascript') === 10)
+console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 
 // Q4
 // Write a function called `greaterThan` that takes
@@ -47,8 +58,9 @@
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
 
-
-
+console.assert(greaterThan(2, 1) === true)
+console.assert(greaterThan(23, 23) === false)
+console.assert(greaterThan(-234, 1) === false)
 
 // Q5
 // Write a function called `greet` that takes a
@@ -56,8 +68,9 @@
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
-
-
+console.assert(greet("Michael") === "Hello, Michael!")
+console.assert(greet("R2D2") === "Hello, R2D2!")
+console.assert(greet("Iron Man") === "Hello, Iron Man!")
 
 // Q6
 // Write a function called `madlib` that takes
@@ -69,9 +82,6 @@
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
-
-
-
 
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
