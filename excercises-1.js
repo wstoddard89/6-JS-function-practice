91e99cbd35e14994be5c99f05f1d00d7b7af8438
Will Stoddard <wstoddard89@gmail.com>
2020-09-01 17:48:08 -0700

    finish first two exercises

--- excercises-1.js
+++ excercises-1.js
@@ -20,37 +20,48 @@
 ///////////////////////////////////////////////////////
 
 // READ FIRST
-// The assertions are there only to help you make sure 
+// The assertions are there only to help you make sure
 // your functions are written correctly. There is no need
 // to change them. If you write your function correctly,
 // the assertions will do nothing. If you write your function
 // incorrectly or not at all, your assertions will fail
 // in your console.
 
-
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
-console.assert(sum(2,4) === 6)
-console.assert(sum(45, 23) === 68)
-console.assert(sum(-2, -57) === -59)
+function sum(a, b) {
+  return a + b
+}
+
+console.log(sum(2, 4))
+console.log(sum(45, 23))
+console.log(sum(-2, -57))
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-console.assert(avg(2,3,4) === 3)
-console.assert(avg(234, 63, 234) === 177)
-console.assert(avg(123,234,345) === 234)
+function avg(a, b, c) {
+  return (a + b + c) / 3
+}
+
+console.log(avg(2, 3, 4))
+console.log(avg(234, 63, 234))
+console.log(avg(123, 234, 345))
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
-console.assert(getLength('Michael') === 7)
-console.assert(getLength('Javascript') === 10)
-console.assert(getLength('supercalifragilisticexpialidocious') === 34)
+function getLength(string) {
+  return string.length
+}
+
+console.log(getLength("Michael"))
+console.log(getLength("Javascript"))
+console.log(getLength("supercalifragilisticexpialidocious"))
 
 // Q4
 // Write a function called `greaterThan` that takes
@@ -58,9 +69,17 @@ console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 // parameter is greater than the second. Otherwise
 // the function should return `false`.
 
-console.assert(greaterThan(2, 1) === true)
-console.assert(greaterThan(23, 23) === false)
-console.assert(greaterThan(-234, 1) === false)
+function greaterThan(a, b) {
+  if (a > b) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(greaterThan(2, 1))
+console.log(greaterThan(23, 23))
+console.log(greaterThan(-234, 1))
 
 // Q5
 // Write a function called `greet` that takes a
@@ -68,13 +87,17 @@ console.assert(greaterThan(-234, 1) === false)
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
-console.assert(greet("Michael") === "Hello, Michael!")
-console.assert(greet("R2D2") === "Hello, R2D2!")
-console.assert(greet("Iron Man") === "Hello, Iron Man!")
+function greet(name) {
+  console.log("Hello, " + name + "!")
+}
+
+console.log(greet("Michael") === "Hello, Michael!")
+console.log(greet("R2D2") === "Hello, R2D2!")
+console.log(greet("Iron Man") === "Hello, Iron Man!")
 
 // Q6
 // Write a function called `madlib` that takes
-// 4 or more parameters (words). The function 
+// 4 or more parameters (words). The function
 // should insert the words into a pre-defined
 // sentence. Finally the function should return
 // that sentence.
@@ -83,7 +106,11 @@ console.assert(greet("Iron Man") === "Hello, Iron Man!")
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 
-console.assert(madlib("quick","fox","jumps","fence")=== "The quick brown fox jumps over the fence.")
-console.assert(madlib("fat","rabbit","hops","wall")=== "The fat brown rabbit hops over the wall.")
+function madLib(a, b, c, d) {
+  return "The " + a + " brown " + b + " " + c + " over " + " the " + d + "."
+}
+
+console.log(madLib("quick", "fox", "jumps", "fence"))
+console.log(madLib("fat", "rabbit", "hops", "wall"))
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
--- excercises-2.js
+++ excercises-2.js
@@ -4,18 +4,30 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
-console.assert(max(2,3) === 3)
-console.assert(max(23,24) === 24)
-console.assert(max(-23, 12) === 12)
+function max(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
+
+console.log(max(2,3))
+console.log(max(23,24))
+console.log(max(-23, 12))
 
 // ---------------------
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
 
-console.assert(maxOfThree(2,56,3) === 56)
-console.assert(maxOfThree(12,3,4) === 12)
-console.assert(maxOfThree(-12,4,-5) === 4)
+function maxOfThree(a, b, c) {
+  return Math.max(a, b, c)
+}
+
+console.log(maxOfThree(2,56,3))
+console.log(maxOfThree(12,3,4))
+console.log(maxOfThree(-12,4,-5))
 
 // ---------------------
 // Write a function isVowel() that takes a character (i.e. a 
@@ -23,12 +35,21 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // vowel, false otherwise.
 // ---------------------
 
-console.assert(isVowel(0) === false);
-console.assert(isVowel("B") === false);
-console.assert(isVowel("b") === false);
-console.assert(isVowel("a") === true);
-console.assert(isVowel("E") === true);
-console.assert(isVowel("2") === false)
+function isVowel(a) {
+  if (a == "a" || a == "e" || a == "i" || a == "o" || a == "u" || a == "E") {
+    return true
+  } else {
+    return false
+  }
+  
+}
+
+console.log(isVowel(0));
+console.log(isVowel("B"));
+console.log(isVowel("b"));
+console.log(isVowel("a"));
+console.log(isVowel("E"));
+console.log(isVowel("2"))
 
 // ---------------------
 // Write a function rovarspraket() that will translate 
@@ -37,6 +58,10 @@ console.assert(isVowel("2") === false)
 // For example, translate("this is fun") should return 
 // the string "tothohisos isos fofunon".
 // ---------------------
+
+function rovarspraket() {
+
+}
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -51,6 +76,15 @@ console.assert(rovarspraket(0) === "0")
 // return the string "ratset gaj".
 // ---------------------
 
+function reverse(a) {
+  let reversed = ""
+  for (let i = a.length - 1; i >= 0; i--) {
+    reversed =+ a[i]
+  }
+  return reversed
+} 
 
-console.assert(reverse("books") === "skoob")
-console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+
+
+console.log(reverse("books"))
+console.log(reverse("we don't want no trouble"))
